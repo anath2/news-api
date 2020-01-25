62579c7e8cda168ba3b7369c9f56ee698dbbfcf0
Ajit Nath <ajit.nath@organicgrowthfunds.com>
2020-01-25 23:54:52 +0800

    Sets up application and celery factory

--- news_api/__init__.py
+++ news_api/__init__.py
@@ -9,22 +9,26 @@ from flask_restful import Api
 from . import resources
 
 
-def create_app(test_config: Optional[Dict] = None):
+def create_app(
+        app_name: str = __name__,
+        test_config: Optional[Dict] = None
+):
     '''
     ARGS:
+        app_name: Application name
         test_config: A dictionary with test configurations
 
     Flask application factory
     '''
     app = Flask(__name__, instance_relative_config=True)
 
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'dev.sqlite')
-    )
+    config_map = {
+        'development': 'dev_config.py',
+        'production': 'prod_config.py'
+    }
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile(config_map[app.env])
     else:
         app.config.from_mapping(test_config)
 
@@ -42,3 +46,34 @@ def create_app(test_config: Optional[Dict] = None):
     db.init_app(app)
 
     return app
+
+
+def create_celery(app: Flask, celery_name: str = __name__):
+    '''
+    ARGS:
+        app: Flask application instance
+        celery_name: Celery app name
+
+    Creates a celery instance and sets up applicatio context
+    '''
+    instance = Celery(
+        name=celery_name,
+        broker=app.config['CELERY_BROKER'],
+        backend=app.config['CELERY_BACKEND']
+    )
+
+    class ContextTask(instance.Task):
+        '''
+        Inherits tasks and creates a cache.
+        Also, wraps application context around call to task
+        '''
+
+        def __init__(self):
+            self._cache = Redis.from_url(app.config['CELERY_BACKEND'])
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return super().__call(*args, **kwargs)
+
+    instance.Task = ContextTask
+    return instance
